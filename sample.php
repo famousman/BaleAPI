c684efb391c99f45d3de638e87fd6c729144a39a
Saeb Khanzadeh <famousman@users.noreply.github.com>
2019-10-09 14:17:35 +0330

    Update Sample Useful Commands

--- sample.php
+++ sample.php
@@ -1,9 +1,35 @@
 <?php 
-include("BaleAPIv2.php");
-$token="{token}";
+include "BaleAPIv2.php";
+$token="write token here";
 $bot=new balebot($token);
 $chat_id=$bot->ChatID();
-$Text="Ta Da";
-$content=array("chat_id" =>$chat_id,"text" =>$Text);
+$user_id=$bot->UserID();
+$Text_orgi=$bot->Text();
+// send a text message 
+$content=array("chat_id" =>$chat_id,"text" =>"just text message");
+$bot->sendText($content);
+// send an invoice order
+$content=array("chat_id" =>$chat_id,"title" =>"Order Title","description" =>"Order Description","provider_token" =>"Iran Credit Card Number","payload" => "value for return to bot", "prices" => 50000 );
+$response=$bot->sendInvoice($content);
+// send a audio message
+$contenttmp = array('chat_id' => $chat_id, 'audio' => "file_id", 'caption' => "caption");
+$bot->sendAudio($contenttmp);
+// send a photo message
+$contenttmp = array('chat_id' => $chat_id, 'photo' => "file_id", 'caption' => "caption");
+$bot->sendPhoto($contenttmp);
+// send a video message
+$contenttmp = array('chat_id' => $chat_id, 'video' => "file_id", 'caption' => "caption");
+$bot->sendVideo($contenttmp);
+// send a voice message
+$contenttmp = array('chat_id' => $chat_id, 'voice' => "file_id", 'caption' => "caption");
+$bot->sendVoice($contenttmp);
+// send a document message
+$contenttmp = array('chat_id' => $chat_id, 'document' => "file_id", 'caption' => "caption");
+$bot->sendDocument($contenttmp);
+// send a Location message
+$contenttmp = array('chat_id' => $chat_id, 'phone_number' => "phone_number", 'first_name' => "first_name");
+$bot->sendContact($contenttmp);
+// send a Type of message to the chat environment
+$content=array("chat_id" =>$chat_id,"text" =>$bot->MessageTYPE());
 $bot->sendText($content);
 ?>
